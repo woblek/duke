6e1c17ef440d5ce58865b6ac56a12814e34f97ce
woblek <42810002+woblek@users.noreply.github.com>
2019-08-29 15:03:02 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "     ____        _        \n"
+                + "    |  _ \\ _   _| | _____ \n"
+                + "    | | | | | | | |/ / _ \\\n"
+                + "    | |_| | |_| |   <  __/\n"
+                + "    |____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("    Hello from\n" + logo);
+        System.out.println("    __________________________________________________________________________________________\n");
+        System.out.println("    Hello! I'm Duke\n");
+        System.out.println("    What can I do for you?\n");
+        System.out.println("    __________________________________________________________________________________________\n");
+
+
+        String s = "null";
+//        while(!s.equals("bye")){
+        while(true){
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            s = myObj.nextLine();  // Read user input
+
+
+            if (s.equals("bye")){
+                System.out.println("    __________________________________________________________________________________________\n");
+                System.out.println("    Bye. Hope to see you again soon!\n");
+                System.out.println("    __________________________________________________________________________________________\n");
+                return;
+            }
+
+            System.out.println("    __________________________________________________________________________________________\n");
+            System.out.println("    " + s + "\n");  // Output user input
+            System.out.println("    __________________________________________________________________________________________\n");
+
+        }
     }
 }
