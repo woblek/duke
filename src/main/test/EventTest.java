a8286bab83af72f478d1c02fe974aa346251c91c
woblek <woblek@gmail.com>
2019-09-11 19:26:25 +0800

    create EventTest class to test Event class

---
+++ src/main/test/EventTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventTest {
+
+    @Test
+    void testToString() {
+        Integer[] date = {05,10,2019,1800};
+        String eventName = "watch movies with friends";
+        Event event = new Event(eventName, date);
+        assertEquals("[E][✘] watch movies with friends (at: 5/10/2019 1800)", event.toString());
+    }
+
+    @Test
+    void getDate() {
+        Integer[] date = {05,10,2019,1800};
+        String eventName = "watch movies with friends";
+        Event event = new Event(eventName, date);
+        assertEquals("5/10/2019 1800", event.getDate());
+    }
+}
