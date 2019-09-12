0e22dfd006ab506ae9dea67ea7ba6aadb7af9944
woblek <woblek@gmail.com>
2019-09-12 14:41:31 +0800

    add class DeadlineTest.java

---
+++ src/test/java/DeadlineTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeadlineTest {
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getDate() {
+    }
+}
